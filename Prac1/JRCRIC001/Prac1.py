3acb941eb3a6e3b7a6537cf5ea7b8b114b2f8171
Ricky <rickyjericevich@gmail.com>
2019-07-26 16:55:49 +0200

    3 LED binary counter using buttons

--- Prac1/JRCRIC001/Prac1.py
+++ Prac1/JRCRIC001/Prac1.py
@@ -3,32 +3,52 @@
 Name: Ricky Jericevich
 Student Number: JRCRIC001
 Prac: Prac 1
-Date: <23/07/2019>
+Date: <26/07/2019>
 """
 
 # import relevant librares
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+from itertools import product
 
-# initialise GPIOs
+# initialise pin mode
 GPIO.setmode(GPIO.BCM)
-led1 = 17	# pin 17 is LED
-GPIO.setup(led1, GPIO.OUT)
-btn1 = 23	# pin 23 is button
-GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initialise pin connected to button
 
-def my_callback(channel):
-    GPIO.output(led1, not GPIO.input(led1))	# pin changes state, LED changes state
+# initialise buttons
+btns = 23, 24
+GPIO.setup(btns, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# initialise LEDs
+leds = 17, 27, 22
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)	# ensure LEDs are off
+
+# declare global variables
+count = 0
+list = list(product([0, 1], repeat = 3))
+
+# when button 1 is pressed
+def btn1_pressed(channel):
+    global count, list
+    count = 0 if count == 7 else count + 1	# count goes from 7 to 0 instead of 8
+    GPIO.output(leds, list[count])		# LEDs display the count in binary
+
+# when button 2 is pressed
+def btn2_pressed(channel):
+    global count, list
+    count = 7 if count == 0 else count - 1	# count goes from 0 to 7 instead of -1
+    GPIO.output(leds, list[count])		# LEDs display the count in binary
 
 def main():
-    time.sleep(10) # wait for  button press
+    sleep(10)	# wait for button press
 
-# run the functions if
+# run main if
 if __name__ == "__main__":
     try:
-        # interrupt to detect button press
-        GPIO.add_event_detect(btn1, GPIO.RISING, callback=my_callback, bouncetime = 200)
-        # repeat main function
+        # interrupts to detect button press
+        GPIO.add_event_detect(btns[0], GPIO.RISING, callback = btn1_pressed, bouncetime = 200)
+        GPIO.add_event_detect(btns[1], GPIO.RISING, callback = btn2_pressed, bouncetime = 200)
+	# repeat main function
         while True:
             main()
     # correctly stop GPIOs
