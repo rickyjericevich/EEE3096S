b285a577850581bd7522bdef7a0a78d3be2f1982
Ricky <rickyjericevich@gmail.com>
2019-07-26 15:03:24 +0200

    A button toggles an LED

--- Prac1/JRCRIC001/Prac1.py
+++ Prac1/JRCRIC001/Prac1.py
@@ -9,26 +9,32 @@ Date: <23/07/2019>
 # import relevant librares
 import RPi.GPIO as GPIO
 import time
+
+# initialise GPIOs
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+led1 = 17	# pin 17 is LED
+GPIO.setup(led1, GPIO.OUT)
+btn1 = 23	# pin 23 is button
+GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initialise pin connected to button
+
+def my_callback(channel):
+    GPIO.output(led1, not GPIO.input(led1))	# pin changes state, LED changes state
 
 def main():
-    GPIO.output(17, 1)		   #pin goes high, LED turns on
-    while True:			   #repeatedly turn LED on/off
-        state = GPIO.input(17)	   #determine if pin is high or low
-        time.sleep(1) 		   #wait 1 second
-        GPIO.output(17, not state) #pin changes state, LED changes state
+    time.sleep(10) # wait for  button press
 
-# Only run the functions if 
+# run the functions if
 if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
     try:
+        # interrupt to detect button press
+        GPIO.add_event_detect(btn1, GPIO.RISING, callback=my_callback, bouncetime = 200)
+        # repeat main function
         while True:
             main()
+    # correctly stop GPIOs
     except KeyboardInterrupt:
-        print("Exiting gracefully")
-        # Turn off your GPIOs here
         GPIO.cleanup()
+        print("Exiting gracefully")
     except Exception as e:
         GPIO.cleanup()
         print("Some other error occurred")
