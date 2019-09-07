74c39c6735eeb6dc1a1187d37ca1b949fa4dbf11
Ricky <rickyjericevich@gmail.com>
2019-09-07 21:01:02 +0200

    Fixed debouncing, noise still baill bad

--- Prac4/src/Prac4.cpp
+++ Prac4/src/Prac4.cpp
@@ -18,50 +18,56 @@
 
 using namespace std;
 
-bool playing = true;       // should be set false when paused
-bool stopped = false;      // If set to true, program should close
+bool playing = true;       // Set false when paused
+bool stopped = false;      // If set to true, program closes
 unsigned char buffer[2][BUFFER_SIZE][2];
 int buffer_location = 0;
-bool bufferReading = 0;   //using this to switch between column 0 and 1 - the first column
-bool threadReady = false; //using this to finish writing the first column at the start of the song, before the column is played
+bool bufferReading = 0;   // Using this to switch between column 0 and 1 - the first column
+bool threadReady = false; // Using this to finish writing the first column at the start of the song, before the column is played
+long lastInterruptTime = 0;
 
-// Configure your interrupts here.
-// Don't forget to use debouncing.
+// Configure interrupts using debouncing
 void play_pause_isr(void){
-    //Write your logic here
-    delay(500);
-    playing = !playing;
-    (playing) ? printf("Playing\n") : printf("Paused\n");
+    // Debounce
+    long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > 200){
+   	playing = !playing;
+   	(playing) ? printf("Playing\n") : printf("Paused\n");
+    }
+    lastInterruptTime = interruptTime;
 }
 
 void stop_isr(void){
-    // Write your logic here
-    delay(500);
-    printf("Stopped\n");
-    stopped = !stopped;
-    exit(0);
+    // Debounce
+    long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > 200){
+	stopped = !stopped;
+	printf("Stopped\n");
+    }
+    lastInterruptTime = interruptTime;
 }
 
 // Setup Function. Called once
 int setup_gpio(void){
-    //Set up wiring Pi
+    // Set up wiring Pi
     wiringPiSetup();
 
-    //setting up the buttons
+    // Setting up the buttons
     pinMode(PLAY_BUTTON, INPUT);
     pullUpDnControl(PLAY_BUTTON, PUD_UP);
     pinMode(STOP_BUTTON, INPUT);
     pullUpDnControl(STOP_BUTTON, PUD_UP);
     printf("BTNs done\n");
 
-    //setting up the button interrupts
+    // Setting up the button interrupts
     if (wiringPiISR(PLAY_BUTTON, INT_EDGE_FALLING, &play_pause_isr) < 0){
 	printf("Play Button ISR error\n");
     }
     if (wiringPiISR(STOP_BUTTON, INT_EDGE_FALLING, &stop_isr) < 0){
 	printf("Stop Button ISR error\n");
     }
-    //setting up the SPI interface
+
+    // Setting up the SPI interface
     wiringPiSPISetup(SPI_CHAN, SPI_SPEED);
     printf("SPI setup done\n");
     return 0;
@@ -69,33 +75,35 @@ int setup_gpio(void){
 
 /* Thread that handles writing to SPI
  *
- * You must pause writing to SPI if not playing is true (the player is paused)
- * When calling the function to write to SPI, take note of the last argument.
- * You don't need to use the returned value from the wiring pi SPI function
- * You need to use the buffer_location variable to check when you need to switch buffers
+ * Pause writing to SPI if not playing is true (the player is paused).
+ * Using the buffer_location variable to check when it needs to switch buffers
  */
 void *playThread(void *threadargs){
     // If the thread isn't ready, don't do anything
     while(!threadReady){
         continue;
     }
-    //You need to only be playing if the stopped flag is false
-    printf("Reading from buffer %d\n", bufferReading);
+
+    // Playing if the stopped flag is false
     while(!stopped){
-        //Code to suspend playing if paused
+        // Suspend playing if paused
 	while (!playing){
 	    continue;
 	}
 
+	// Write buffer to DAC using SPI
 	wiringPiSPIDataRW(SPI_CHAN, buffer[bufferReading][buffer_location], 2);
 
+	// Check if buffers need to be toggled
 	buffer_location++;
 	if (buffer_location >= BUFFER_SIZE){
 	    buffer_location = 0;
 	    bufferReading = !bufferReading;
 	}
     }
-    printf("Exiting thread\n");
+
+    // Exit program when stopped
+    exit(0);
     pthread_exit(NULL);
 }
 
@@ -106,39 +114,32 @@ int main(){
     }
 
     /* Initialize thread with parameters
-     * Set the play thread to have a 99 priority
-     * Read https://docs.oracle.com/cd/E19455-01/806-5257/attrib-16/index.html
+     * Play thread has a 99 priority
      */
-
-    //Write your logic here
     pthread_attr_t tattr;
     pthread_t thread_id;
     int newprio = 99;
     sched_param param;
 
     pthread_attr_init (&tattr);
-    pthread_attr_getschedparam (&tattr, &param); // safe to get existing scheduling param
-    param.sched_priority = newprio;              // set the priority; others are unchanged
-    pthread_attr_setschedparam (&tattr, &param); // setting the new scheduling param
-    pthread_create(&thread_id, &tattr, playThread, (void *)-1); // with new priority specified
-    printf("Thread initialised\n");
+    pthread_attr_getschedparam (&tattr, &param); // Safe to get existing scheduling param
+    param.sched_priority = newprio;              // Set the priority; others are unchanged
+    pthread_attr_setschedparam (&tattr, &param); // Setting the new scheduling param
+    pthread_create(&thread_id, &tattr, playThread, (void *)-1); // With new priority specified
+
     /* Read from the file, character by character
-     * You need to perform two operations for each character read from the file
-     * You will require bit shifting
-     *
-     * buffer[bufferWriting][counter][0] needs to be set with the control bits
-     * as well as the first few bits of audio
-     *
-     * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
-     *
-     * Don't forget to check if you have pause set or not when writing to the buffer
+     * Using bit shifting
+     * buffer[bufferWriting][counter][0] is set with the control bits
+     * as well as the first few bits of audio.
+     * buffer[bufferWriting][counter][1] is set with the last audio bits
+     * Check if you pause is set or not when writing to the buffer
      */
 
     // Open the file
-    char ch;
+    unsigned char ch;
     FILE *filePointer;
     printf("%s\n", FILENAME);
-    filePointer = fopen(FILENAME, "r"); // read mode
+    filePointer = fopen(FILENAME, "r"); // Read mode
 
     if (filePointer == NULL) {
         perror("Error while opening the file\n");
@@ -147,11 +148,26 @@ int main(){
 	printf("File opened\n");
     }
 
-    int counter = 0;
-    int bufferWriting = 0;
-    printf("Writing to buffer %d\n", bufferWriting);
+    int counter = 0, bufferWriting = 0, c = 0;
+    unsigned char chararr[150];
+
     // Have a loop to read from the file
     while((ch = fgetc(filePointer)) != EOF){
+	// EOF isnt working on the pi, so manually check for end-of-file
+	chararr[c++] = ch;
+	if (c == 150){
+	    int num = 0;
+	    for (int i = 0; i < c; i++){
+		if (chararr[i] == 129){
+		    num++;
+		}
+	    }
+	    if (num == c){
+		stopped = !stopped;
+	    }
+	    c = 0;
+	}
+
         while(threadReady && bufferWriting==bufferReading && counter==0){
             //waits in here after it has written to a side, and the thread is still reading from the other side
             continue;
@@ -163,25 +179,20 @@ int main(){
 
         counter++;
         if(counter >= BUFFER_SIZE+1){
-	    printf("Buffer %d is full, ", bufferWriting);
             if(!threadReady){
-		printf("start playing\n");
                 threadReady = true;
             }
 
             counter = 0;
-            bufferWriting = (bufferWriting + 1)%2;
-	    printf("writing to buffer %d\n", bufferWriting);
+            bufferWriting = !bufferWriting;
         }
     }
 
     // Close the file
     fclose(filePointer);
-    printf("Complete reading");
 
     //Join and exit the playthread
     pthread_join(thread_id, NULL);
     pthread_exit(NULL);
-    printf("Exiting program");
     return 0;
 }
