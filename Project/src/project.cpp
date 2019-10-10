2460fbdd6fb13e1eebf6e422d114da1041522c77
rickyjericevich <53138411+rickyjericevich@users.noreply.github.com>
2019-10-10 16:51:12 +0200

    Update project.cpp

--- Project/src/project.cpp
+++ Project/src/project.cpp
@@ -6,13 +6,12 @@ usigned short int light = 0, temp = 0, humidity = 0;
 char delay = 1;
 int sysTime[] = {0, 0, 0};
 
-int initPeriphs(void){
-
+int initPeriphs(){
     printf("Setting up...\n");
 
     wiringPiSetup();
 
-    RTC = wiringPiI2CSetup(RTCAddr); //Set up the RTC
+    //RTC = wiringPiI2CSetup(RTCAddr); //Set up the RTC
 
     //Set up the buttons
     for(int i; i < sizeof(BTNS)/sizeof(BTNS[0]); i++){ 
@@ -40,12 +39,45 @@ int initPeriphs(void){
     }
     printf("BTNS done\n"); 
 
-    // Setting up the SPI interface
-    wiringPiSPISetup(SPI_CHAN0, SPI_SPEED);
-    wiringPiSPISetup(SPI_CHAN1, SPI_SPEED);
+    // Setting up SPI
+    wiringPiSPISetup(SPI_CHAN0, SPI_SPEED);//DAC
+    wiringPiSPISetup(SPI_CHAN1, SPI_SPEED);//ADC
     printf("SPI setup done\n");
 
     printf("Setup done\n");
 
     return 0;
+}
+
+int main(){
+    // Call the setup GPIO function
+    if(initPeriphs() == -1){
+        return 0;
+    }
+    initThread();
+    
+    while (1){
+        if (!paused){
+            
+        }
+    }
+}
+
+void initThread(){
+    pthread_attr_t tattr;
+    pthread_t thread_id;
+    int newprio = 99;
+    sched_param param;
+    
+    pthread_attr_init (&tattr);
+    pthread_attr_getschedparam (&tattr, &param); // Safe to get existing scheduling param
+    param.sched_priority = newprio;              // Set the priority; others are unchanged
+    pthread_attr_setschedparam (&tattr, &param); // Setting the new scheduling param
+    pthread_create(&thread_id, &tattr, dataThread, (void *)-1); // With new priority specified
+}
+
+void *dataThread(void *threadargs){
+    while(1){
+        
+    }
 }
